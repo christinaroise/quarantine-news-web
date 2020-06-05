8225a6915736f515dac67cee27feebd4efe9bb4a
christinaroise <54266462+christinaroise@users.noreply.github.com>
2020-06-05 09:28:16 +0200

    Updates

---
+++ precache-manifest.b87a13362f0539eb81410066cbc9c48e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "576d6790419787b4300ea362a9e26787",
+    "url": "/quarantine-news-web/index.html"
+  },
+  {
+    "revision": "5ec83d4822013b66231d",
+    "url": "/quarantine-news-web/static/css/main.c90e8df2.chunk.css"
+  },
+  {
+    "revision": "2cf8ca860583f332b206",
+    "url": "/quarantine-news-web/static/js/2.370982f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quarantine-news-web/static/js/2.370982f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ec83d4822013b66231d",
+    "url": "/quarantine-news-web/static/js/main.8f12fba8.chunk.js"
+  },
+  {
+    "revision": "9bf17ddd2a36916e053f",
+    "url": "/quarantine-news-web/static/js/runtime-main.01ed3c0b.js"
+  },
+  {
+    "revision": "747a466b381d87c957647684e49ce303",
+    "url": "/quarantine-news-web/static/media/001-coronavirus.747a466b.png"
+  },
+  {
+    "revision": "529ba738bf9bb64d6a60d62448c780b6",
+    "url": "/quarantine-news-web/static/media/TermsOfService.529ba738.pdf"
+  },
+  {
+    "revision": "cf804de69beb031e3530c5d72759dc30",
+    "url": "/quarantine-news-web/static/media/browse.cf804de6.png"
+  },
+  {
+    "revision": "fda11aace0e36112710033296ddbc7e5",
+    "url": "/quarantine-news-web/static/media/dashboard.fda11aac.png"
+  },
+  {
+    "revision": "16580c533d2977875ae4d61018b0e842",
+    "url": "/quarantine-news-web/static/media/filter.16580c53.png"
+  },
+  {
+    "revision": "c8e2d07f781975f43f661cb68ba435a3",
+    "url": "/quarantine-news-web/static/media/library.c8e2d07f.png"
+  }
+]);
